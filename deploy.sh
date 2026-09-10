6b32b9b1fed2280150c544ef31340e3cff1fc43a
Kiran Sankar Das <kiran.sankar.das@gmail.com>
2026-09-10 08:20:59 +0530

    fix: correct UUID format in metadata and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-NAME=MacLike(Work)Spaces
-UUID=MacLike(Work)Spaces@kiran86.com
-OLD_UUID=MaximizeWindowIntoNewWorkspace@kyleross.com
+NAME='MacLike(Work)Spaces'
+UUID='MacLike(Work)Spaces@kiran86.com'
+OLD_UUID='MaximizeWindowIntoNewWorkspace@kyleross.com'
+LEGACY_UUID='MacLikeWorkSpaces@kiran86.com'
+LEGACY_OLD_UUID='MaximizeWindowLikeMac@kiran86.com'
 EXTENSIONS_DIR="$HOME/.local/share/gnome-shell/extensions"
 
 mkdir -p "$EXTENSIONS_DIR"
-# Avoid running the original and renamed extensions together.
+# Avoid running the original, renamed, and legacy variants together.
 rm -rf "$EXTENSIONS_DIR/$OLD_UUID"
+rm -rf "$EXTENSIONS_DIR/$LEGACY_UUID"
+rm -rf "$EXTENSIONS_DIR/$LEGACY_OLD_UUID"
 rm -rf "$EXTENSIONS_DIR/$UUID"
 cp -r "$NAME" "$EXTENSIONS_DIR/$UUID"
